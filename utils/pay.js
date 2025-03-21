62ef35339b31d56e6205598966673a290bac3403
TOD-nights <xu1271669848@gmail.com>
2025-03-22 03:47:56 +0800

    Update pay.js

--- utils/pay.js
+++ utils/pay.js
@@ -100,7 +100,9 @@ function wxpay(type, money, orderId, redirectUrl, data) {
 
     }//未来店
     else if (data.shopInfo.id==1){
+
       sn = '32EL21088705948'
+
     }
     //如果没有打印机，则返回
     if(!sn){
