b38a58c6be9d45ff101397334685dce11b8df925
TOD-nights <xu1271669848@gmail.com>
2025-03-24 20:04:37 +0800

    Update pay.js

--- utils/pay.js
+++ utils/pay.js
@@ -172,7 +172,7 @@ for(let i=0;i<data.goodsList.length;i++){
       }
       //未来店
     else if (data.shopInfo.id==1){
-      sn = '742N30GDRND8E4As'
+      sn = '742N30GDRND8E4A'
 
     }
       //如果没有打印机，则返回
